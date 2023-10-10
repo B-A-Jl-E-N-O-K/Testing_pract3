ed0e021dc22fe7e0bcc4c6ca5e8ef11f9d401003
BAJlENOK <logggistic@mail.ru>
2023-10-10 22:45:34 +0300

    added 3 functions in guesser class - init, getOpened, openChar

--- guesser.py
+++ guesser.py
@@ -2,16 +2,19 @@
 
 class Guesser:
     def __init__(self, word):
-        
+        self.word = word
+        self.opened = ['?' for _ in range(len(word))]        
 
     def is_geussed(self):
         
 
     def open_char(self, char):
-        
+        for i in range(len(self.word)):
+            if self.word[i] == char:
+                self.opened[i] = self.word[i]        
 
     def get_opened(self):
-        
+        return ''.join(self.opened)
 
     def get_word(self):
         
